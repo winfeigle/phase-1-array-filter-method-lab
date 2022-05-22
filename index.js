15427c2ce1d78f0c7a0f3b717761f5f2227d3a46
Win Feigle <win.feigle@gmail.com>
2022-05-22 12:38:02 -0600

    commit tree 2fdd2ba07b376016d5f66fe2bf27687c98e27e7c

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Code your solution here
+const findMatching = (arr, query) => {
+    return arr.filter(element => element.toLowerCase() === query.toLowerCase());
+}
+
+const fuzzyMatch = (arr, query) => {
+    return arr.filter(element => element.slice(0, query.length).toLowerCase() === query.toLowerCase());
+}
+
+const matchName = (arr, query) => arr.filter(driver => driver.name === query);
